24d40270db9f38fa3b14eb0b4d2e9c6edef3ce27
Ben Glasser <ben@benglasser.com>
2013-06-01 21:04:10 -0700

    fix run-query

--- src/queryizer/controller.clj
+++ src/queryizer/controller.clj
@@ -15,16 +15,17 @@
 
 (def query-finished (promise))
 ;;mmake UUID for query result
-(defn- make-id
-  "Makes a UUID for uniquely "
-  []
+(def make-id
+  "Makes a UUID for uniquely"
   (str (java.util.UUID/randomUUID)))
 
 ;;runs given query using korma
 (defn run-query [query]
-  (let [results (exec-raw [query] :results )]
-  (println "QUERY ID: " query)
-  (spit (io/resource "jobs") results) results))
+  (let [results (exec-raw [query] :results) 
+    id make-id]
+    (println "QUERY ID: " query)
+    (spit (io/resource "jobs") 
+      (apply str ["{:id " id " :results " results "}"])) results))
 
 ;;reads in "resources/queries"
 (def available-queries
@@ -36,6 +37,7 @@
   (:sql (first (filter #(= id (:id %)) available-queries))))
 
 (def list-jobs
-  (read-string
-    (slurp (io/resource "jobs"))))
+  (delay
+    (read-string
+      (slurp (io/resource "jobs")))))
 
