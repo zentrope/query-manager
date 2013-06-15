2f5a0d4769cfbd408bf0acf309587c1576854fea
Keith Irwin <keith@zentrope.com>
2013-06-15 08:43:24 -0700

    Client and API stubs, removed old UI.

---
+++ resources/public/client.js
@@ -0,0 +1,14 @@
+// Requires jquery
+$(function () {
+
+  console.log("hello");
+
+  function notImplementedPage() {
+    $("body").html(
+      "<h1>Queryizer App</h1>" +
+        "<p>Client not yet implemented.</p>");
+  }
+
+  notImplementedPage();
+
+});
--- src/queryizer/view.clj
+++ src/queryizer/view.clj
@@ -1,97 +1,94 @@
 (ns queryizer.view
-  (:use compojure.core)
-  (:use hiccup.core)
-  (:use queryizer.controller)
   (:require [clojure.data.json :as json]
+            [compojure.core :refer [defroutes GET POST DELETE PUT]]
             [compojure.handler :as handler]
             [compojure.route :refer [resources not-found]]
-            [ring.util.response :refer [redirect]]))
+            [ring.util.response :refer [redirect]]
+            [hiccup.page :refer [html5 include-js include-css]]))
 
-(defn view-layout [& content]
-  (html
-    [:head [:meta {:http-equiv "Content-type"
-    :content "text/html; charset=utf-8"}]
-    [:link {:href "styles.css" :type "text/css" :rel "stylesheet"}]
-    [:title "Queryizer"]]
-    [:div {:id "container"} [:body content]]))
-
-;;hyperlinked list of predefined queries
-(defn- query-selection
-  []
-  (list
-   [:h2 "Available queries"]
-    (for
-      [elem (queryizer.controller/available-queries)]
-      (list
-        [:a {:href (str "/jobs/" (:id elem))}
-        (:id elem)] [:br ]))))
-
-;;hyperlinked list of predefined queries
-(defn- long-query-selection
-  []
-  (list [:h2 "Long queries"]
-    (for
-      [elem (queryizer.controller/available-queries)]
-      (list
-        [:a {:href (str "/long/" (:id elem))}
-        (:id elem)] [:br ]))))
-
-(defn view-input []
-  (view-layout
-    [:h2 "Enter query"]
-
-    [:form {:method "post" :action "/"}
-    [:input.math {:type "text" :name "query"}] [:br ]
-    [:input.action {:type "submit" :value "Enter"}]]
-    (query-selection)
-    (long-query-selection)))
-
-(defn view-jobs []
-  (println "view-jobs")
-  (view-layout
-    [:h2 "Current Jobs"]
-    (for [job (queryizer.controller/list-jobs)]
-      [:p (str job)])))
-
-(defn view-output [query]
-  (let [rows (queryizer.controller/submit-query query)
-    cols (keys (first rows))]
-    (view-layout
-      [:h2 "Here is your result"]
-      [:table [:tr (for [col cols] [:th (name col)])]
-      (for [row rows] [:tr (for [item (vals row)] [:td item])])]
-      [:a.action {:href "/"} "Enter another?"] [:br ])))
-
-;;Routes------------------------------------------------------------------
 (defroutes main-routes
 
-  (GET "/" []
-    (view-input))
+  ;;---------------------------------------------------------------------------
+  ;; DATABASE API
+  ;;---------------------------------------------------------------------------
 
-  (GET "/tables" [] (view-output "show tables"))
+  (GET "/qzer/api/db/:id"
+      [id]
+    (json/write-str {:error "not implemented"}))
 
-  (GET "/query/:id" [id] (view-output (queryizer.controller/query id)))
+  (GET "/qzer/api/db"
+      []
+    (json/write-str []))
 
-  (GET "/tables/:table" [table] (view-output (str "select * from " table)))
+  (POST "/qzer/api/db"
+      ;; Should be a version of this to upload a file.
+      [query-id]
+    (json/write-str {:error "not implemented"}))
 
-  (POST "/" [query]
-    (println "obvious debug statment: " query)
-    (view-output query))
+  (DELETE "/qzer/api/db/:id"
+      [id]
+    (json/write-str {:error "not implemented"}))
 
-  ;;-------These are the asynch parts...
+  ;;---------------------------------------------------------------------------
+  ;; QUERY API
+  ;;---------------------------------------------------------------------------
 
-  (GET "/jobs" [] (println "GET jobs")
-     (view-jobs))
+  (GET "/qzer/api/query/:id"
+      [id]
+    (json/write-str {:error "not implemented"}))
 
-  (GET "/jobs/:job" [job] (println "GET jobs")
-       (submit-job (query job))
-       (redirect "/jobs"))
-  (GET "/long/:job" [job] (println "LONG jobs")
-       (long-submit-job (query job))
-       (redirect "/jobs"))
+  (GET "/qzer/api/query"
+      []
+    (json/write-str []))
+
+  (POST "/qzer/api/query"
+      ;; Should be a version of this to upload a file.
+      [query-id]
+    (json/write-str {:error "not implemented"}))
+
+  (DELETE "/qzer/api/query/:id"
+      [id]
+    (json/write-str {:error "not implemented"}))
+
+  ;;---------------------------------------------------------------------------
+  ;; JOB API
+  ;;---------------------------------------------------------------------------
+
+  (GET "/qzer/api/job/:id"
+      [id]
+    (json/write-str {:error "not implemented"}))
+
+  (GET "/qzer/api/job"
+      []
+    (json/write-str []))
+
+  (POST "/qzer/api/job/:query-id"
+      [query-id]
+    (json/write-str {:error "not implemented"}))
+
+  (DELETE "/qzer/api/job/:id"
+      [id]
+    (json/write-str {:error "not implemented"}))
+
+  ;;---------------------------------------------------------------------------
+  ;; CLIENT
+  ;;---------------------------------------------------------------------------
+
+  (GET "/"
+      []
+    (redirect "/qzer"))
+
+  (GET "/qzer"
+      []
+    (html5 [:head
+            [:title "Long Running Query Manager"]
+            (include-css "styles.css")
+            (include-js "jquery-2.0.2.min.js")
+            (include-js "client.js")]
+           [:body "Loading..."]))
 
   (resources "/")
-  (not-found "<h1>Oops. Try <a href='/'>here</a>."))
+  (not-found "<h1>Oops. Try <a href='/'>here</a>.</h1>"))
 
 (def app
   (-> main-routes
