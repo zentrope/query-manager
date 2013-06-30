c6957ab7daa28450e0e5cb19350e168bd09d47c9
Keith Irwin <keith@zentrope.com>
2013-06-29 20:29:14 -0700

    More mushing in clojurescript.

--- src-cljs/query-manager/main.cljs
+++ src-cljs/query-manager/main.cljs
@@ -1,37 +1,42 @@
 (ns query-manager.main
-  (:use-macros [dommy.macros :only [sel1 node]])
-  (:require [dommy.core :refer [set-html! html replace! listen!]]
+  (:use-macros [dommy.macros :only [sel sel1 node]])
+  (:require [dommy.core :refer [set-html! replace! listen!]]
             [query-manager.net :refer [dump]]))
 
+(let [c (atom 0)]
+  (defn- counter
+    []
+    (swap! c inc)))
 
-(defn- not-implemented
+(defn- ni-template
   []
   (node [:div#container
          [:h1 "Query Manager App"]
          [:p "Client not yet implemented."]
          [:p [:button#test-button "Test"]]
-         [:p.flash]]))
+         [:p.flash "."]]))
 
 (defn- flash
   [msg]
-  (replace! (sel1 :.flash) msg))
+  (set-html! (sel1 :.flash) (str msg " &bull; " (counter))))
 
 (defn- fake-action
   []
   (let [data [{:id "1" :sql "select * from foo" :description "Blah"}]]
     (dump data
-          (fn [] (flash "Worked!"))
+          (fn [_] (flash "worked"))
           (fn [e] (flash (str "Failed: " (:status e) " -> " (:reason e)))))))
 
-(defn- noimpl-events
+(defn- ni-view
   []
-  (let [ni (not-implemented)]
-    (listen! ni :click fake-action)))
+  (let [ni (ni-template)]
+    (listen! [ni :#test-button] :click fake-action)
+    ni))
 
-(defn ^:export main
+(defn main
   []
   (.log js/console "Hello.")
-  (replace! (sel1 :body) (noimpl-events))
+  (replace! (sel1 :body) (ni-view))
   (.log js/console "Goodbye."))
 
 (set! (.-onload js/window) main)
--- src-cljs/query-manager/net.cljs
+++ src-cljs/query-manager/net.cljs
@@ -1,35 +1,63 @@
 (ns query-manager.net
   (:require [goog.net.XhrIo :as xhr]))
 
+;;-----------------------------------------------------------------------------
+;; Implementation
+;;-----------------------------------------------------------------------------
+
 (defn- jsonify
   [data]
-  (JSON/stringify (clj->js data)))
+  (when data
+    (JSON/stringify (clj->js data))))
 
-(defn- net-delegate
-  [on-success on-failure]
+(defn- success
+  [response f {:keys [type]}]
+  (let [t (.-target response)]
+    (f (let [text (.getResponseText t)]
+         (if (<= (count text) 0)
+           ""
+           (case type
+             :json (.getResponseJson t)
+             text))))))
+
+(defn- failure
+  [response f]
+  (let [t (.-target response)]
+    (f {:status (.getStatus t)
+        :reason (.getStatusText t)})))
+
+(defn- mk-handler
+  [{:keys [on-success on-failure] :as opts}]
   (fn [response]
     (if (.isSuccess (.-target response))
-      (when-not (nil? on-success)
-        (on-success (try
-                      (.getResponseJson (.-target response))
-                      (catch js/Error x
-                          (.getResponseText (.-target response))))))
-      (when-not (nil? on-failure)
-        (on-failure {:status (.getStatus (.-target response))
-                     :reason (.getStatusText (.-target response))})))))
+      (when on-success
+        (success response on-success opts))
+      (when on-failure
+        (failure response on-failure)))))
 
-(defn- net-request
-  [{:keys [uri method on-success on-failure data content-type]}]
-  (let [headers (clj->js {:content-type content-type})
-        handler (net-delegate on-success on-failure)]
+(defn- mimetype
+  [{:keys [type]}]
+  (case type
+    :json "application/json"
+    "plain/text"))
+
+(defn- ajax
+  [& opts]
+  (let [{:keys [uri method data type]} opts
+        headers (clj->js {:content-type (mimetype opts)})
+        handler (mk-handler opts)]
     (.send goog.net.XhrIo uri handler method (jsonify data) headers)))
+
+;;-----------------------------------------------------------------------------
+;; Public
+;;-----------------------------------------------------------------------------
 
 (defn dump
   [data success failure]
-  (net-request
-   {:uri "/qman/api/dump"
-    :method "POST"
-    :on-failure failure
-    :on-success success
-    :data data
-    :content-type "application/json"}))
+  (ajax :uri "/qman/api/dump"
+        :method "POST"
+        :on-failure failure
+        :on-success success
+        :data data
+        :type :json
+        :content-type "application/json"))
