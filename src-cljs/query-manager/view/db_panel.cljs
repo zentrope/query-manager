cd7e35677961775317e3f0e4388f326804c77f60
Keith Irwin <keith@zentrope.com>
2013-07-04 08:20:50 -0700

    Removed net request from db view panel.

--- src-cljs/query-manager/view/db_panel.cljs
+++ src-cljs/query-manager/view/db_panel.cljs
@@ -10,8 +10,7 @@
 (def ^:private template
   (node [:div#container.lister
          [:h2 "Database"]
-         [:div#database-table
-          [:button#db-create "connect"]]]))
+         [:div#database-table]]))
 
 (defn- table-of
   [{:keys [type host port user database]}]
@@ -28,40 +27,19 @@
                 [:td port]
                 [:td user]
                 [:td database]]]
-              [:button#db-edit "edit"])))
+              [:button#dbp-edit "edit"])))
+
+(defn- on-click
+  [broadcast db e]
+  (broadcast [:db-form-show {:value db}]))
 
 (defn- on-update
   [broadcast db]
   (replace-contents! (sel1 :#database-table) (table-of db))
-  (listen! (sel1 :#db-edit) :click (fn [e] (broadcast [:db-form-show {:value db}]))))
-
-(defn- on-error
-  [err]
-  (replace-contents! (sel1 :#database-table)
-                     (node (list [:p "Unable to connect."]
-                                 [:button#db-create "connect"]))))
-
-(defn- on-success
-  [delegate]
-  (fn [db-info]
-    (delegate [:loading {:value false}])
-    (delegate [:db-change {:value (js->clj db-info :keywordize-keys true)}])))
-
-(defn- on-failure
-  [delegate]
-  (fn [error]
-    (delegate [:loading {:value false}])
-    (delegate [:net-error {:value error}])))
-
-(defn- on-click
-  [delegate]
-  (delegate [:loading {:value true}])
-  (net/get-db (on-success delegate)
-              (on-failure delegate)))
+  (listen! (sel1 :#dbp-edit) :click (partial on-click broadcast db)))
 
 (defn- mk-template
   [delegate]
-  (listen! [template :#create-connection] :click (fn [] (on-click delegate)))
   template)
 
 ;;-----------------------------------------------------------------------------
@@ -74,11 +52,10 @@
 
 (defn events
   []
-  [:db-change :net-error])
+  [:db-change])
 
 (defn recv
   [broadcast [topic event]]
   (case topic
     :db-change (on-update broadcast (:value event))
-    :net-error (on-error (:value event))
     true))
